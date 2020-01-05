fa150bc445fdfbd0fde09e311bcca722d1241b69
shimobayashi <akimasashimobayashi@gmail.com>
2020-01-05 18:53:40 +0900

    とりあえず適当にDynamoDBにつないで全件取得するようにした。

--- addguid/app.rb
+++ addguid/app.rb
@@ -1,38 +1,26 @@
-# require 'httparty'
 require 'json'
+require 'aws-sdk'
 
 def lambda_handler(event:, context:)
-  # Sample pure Lambda function
+  dynamodb = Aws::DynamoDB::Client.new(
+    region: 'ap-northeast-1',
+  )
 
-  # Parameters
-  # ----------
-  # event: Hash, required
-  #     API Gateway Lambda Proxy Input Format
-  #     Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+  begin
+    result = dynamodb.scan({ table_name: 'addguid_target' })
+    result.items.each do |target|
+      p target
+    end
 
-  # context: object, required
-  #     Lambda Context runtime methods and attributes
-  #     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-
-  # Returns
-  # ------
-  # API Gateway Lambda Proxy Output Format: dict
-  #     'statusCode' and 'body' are required
-  #     # api-gateway-simple-proxy-for-lambda-output-format
-  #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-
-  # begin
-  #   response = HTTParty.get('http://checkip.amazonaws.com/')
-  # rescue HTTParty::Error => error
-  #   puts error.inspect
-  #   raise error
-  # end
+  rescue  Aws::DynamoDB::Errors::ServiceError => error
+    puts "Unable to read item:"
+    puts "#{error.message}"
+  end
 
   {
     statusCode: 200,
     body: {
       message: "Hello World!",
-      # location: response.body
     }.to_json
   }
 end
