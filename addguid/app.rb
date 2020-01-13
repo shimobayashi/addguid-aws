69eec015259c9bb01fa0e67e754265dbad1d0948
shimobayashi <akimasashimobayashi@gmail.com>
2020-01-13 20:38:58 +0900

    生成したRSSをS3にputするようにした。

--- addguid/app.rb
+++ addguid/app.rb
@@ -3,17 +3,21 @@ require 'aws-sdk'
 require 'feed-normalizer'
 require 'open-uri'
 require 'digest/md5'
+require 'aws-sdk-s3'
+require 'cgi'
 
 def lambda_handler(event:, context:)
   dynamodb = Aws::DynamoDB::Client.new(
     region: 'ap-northeast-1',
   )
 
+  s3 = Aws::S3::Resource.new(region:'ap-northeast-1')
   begin
     result = dynamodb.scan({ table_name: 'addguid_target' })
     result.items.each do |target|
       p target
 
+      #XXX ここでコケると後続の処理が全部コケるのでエラーハンドリングする
       feed = FeedNormalizer::FeedNormalizer.parse open(target['uri'])
       feed.entries.each do |entry|
         # guidに相当する文字列を渡されたオプションからよしなに生成する。
@@ -38,10 +42,13 @@ def lambda_handler(event:, context:)
         entry.id = Digest::MD5.hexdigest(seed)
       end
 
+      key = "production/#{target['uri'].gsub('/', '-')}.xml"
+      link = "https://addguid-aws.s3.ap-northeast-1.amazonaws.com/#{CGI.escape(key)}"
+
       rss = RSS::Maker.make('2.0') do |rss|
         rss.channel.title = feed.title
         rss.channel.description = feed.description
-        rss.channel.link = feed.url #TODO 出力先のURLに置き換えたい
+        rss.channel.link = link
 
         feed.entries.each do |entry|
           item = rss.items.new_item
@@ -54,7 +61,12 @@ def lambda_handler(event:, context:)
         end
       end
 
-      p rss.to_s
+      obj = s3.bucket('addguid-aws').object(key)
+      obj.put(
+        acl: 'public-read',
+        body: rss.to_s,
+        content_type: 'application/rss+xml;charset=UTF-8',
+      )
     end
   rescue  Aws::DynamoDB::Errors::ServiceError => error
     puts "Unable to read item:"
